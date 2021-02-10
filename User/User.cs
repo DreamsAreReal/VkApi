75cef98d28c70c9ffefede04578b9b77de590b2c
ProgWidness <ProgWidness@users.noreply.github.com>
2021-02-10 22:40:49 +0300

    Make user and create tests project

---
+++ User/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+
+namespace User
+{
+    public class User
+    {
+        public CookieContainer CookieContainer { get; private set; }
+
+        public string Login { get; }
+        public string Password { get; }
+
+        public User(string login, string password)
+        {
+            CookieContainer = new CookieContainer();
+            Login = login;
+            Password = password;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var user = (User) obj;
+            if (user == null)
+            {
+                throw new ArgumentException("Obj can't be equal to null");
+            }
+            if (Login == user.Login && Password == user.Password && CookieContainer.Equals(user.CookieContainer))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
